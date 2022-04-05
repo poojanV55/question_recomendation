9d905a09a28cc09a7a8aa06b438034a602dc1c01
Poojan Vadaliya <63559515+poojanV55@users.noreply.github.com>
2022-04-05 15:33:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask,request,jsonify
+from flask_cors import CORS
+import recommendation
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/question', methods=['GET'])
+def recommend_movies():
+    res = recommendation.results(request.args.get('Pawns'))
+    return jsonify(res)
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
+
+
